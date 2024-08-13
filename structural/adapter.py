bb99935499ab4fde7231eb7404904436944fd593
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-13 06:27:00 +0500

    adapter pattern

---
+++ structural/adapter.py
@@ -0,0 +1,51 @@
+# to make an object interface with another
+
+
+class UsbCable:
+    def __init__(self):
+        self.isPlugged = False
+    
+    def plugUsb(self):
+        self.isPlugged = True
+
+class UsbPort:
+    def __init__(self):
+        self.portAvailable = True
+    
+    def plug(self, usb: UsbCable):
+        if self.portAvailable:
+            usb.plugUsb()
+            self.portAvailable = False
+
+class MicroUsb:
+    def __init__(self):
+        self.isPlugged = False
+    
+    def plugMicroUsb(self):
+        self.isPlugged = True
+
+class MicroToUsbAdapter(UsbCable):
+    def __init__(self, microUsb: MicroUsb):
+        self.microUsb = microUsb
+    
+    def plugMicroUsb(self):
+        self.microUsb.plugMicroUsb()
+    
+    def plugUsb(self):
+        self.plugMicroUsb()
+
+
+if __name__ == "__main__":
+    usbCable = UsbCable()
+    microUsb = MicroUsb()
+    usbPort1 = UsbPort()
+    usbPort2 = UsbPort()
+
+    # can plug cable directly
+    usbPort1.plug(usbCable)
+
+    # can't do this
+    # usbPort2.plug(microUsb)
+    
+    # use adapter
+    usbPort2.plug(MicroToUsbAdapter(microUsb))
