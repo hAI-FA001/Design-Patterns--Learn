8b490c6e07f0d9f525a49ab78d7136c5de3302e7
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-08-17 17:38:33 +0500

    command

---
+++ behavioral/CommandPattern.java
@@ -0,0 +1,88 @@
+// turns requests/commands into objects
+// can parametrize or queue commands
+// decouples the Sender and Receiver
+// e.g. if "turn AC on" needed 10 methods to be called, then Sender would need to know and invoke them
+// seems like Facade (hiding complexity) but is different (we turn commands/requests into objects)
+
+package behavioral;
+
+public class CommandPattern {
+    public static void main(String[] args) {
+        AirConditioner ac = new AirConditioner();
+        // sender needs to know the commands
+        ac.turnOnAC();
+        ac.setTemp(24);
+        ac.turnOffAC();
+
+        // sender only needs to call pressButton()
+        RemoteControl remoteControl = new RemoteControl();
+        remoteControl.setCommand(new TurnOnACCommand(ac));
+        remoteControl.pressButton();
+
+        remoteControl.setCommand(new TurnOffACCommand(ac));
+        remoteControl.pressButton();
+    }
+}
+
+class RemoteControl {
+    ICommand command;
+
+    void setCommand(ICommand command) {
+        this.command = command;
+    }
+
+    void pressButton() {
+        command.execute();
+    }
+}
+
+// for each command we want to expose, we make a class like this
+class TurnOnACCommand implements ICommand {
+    AirConditioner ac;
+
+    TurnOnACCommand(AirConditioner ac) {
+        this.ac = ac;
+    }
+
+    @Override
+    public void execute() {
+        ac.turnOnAC();
+    }
+}
+
+class TurnOffACCommand implements ICommand {
+    AirConditioner ac;
+
+    TurnOffACCommand(AirConditioner ac) {
+        this.ac = ac;
+    }
+
+    @Override
+    public void execute() {
+        ac.turnOffAC();
+    }
+}
+
+interface ICommand {
+    void execute();
+}
+
+class AirConditioner {
+    boolean isOn;
+    int temp;
+
+    void turnOnAC() {
+        isOn = true;
+        System.out.println("Turned on AC");
+    }
+
+    void turnOffAC() {
+        isOn = false;
+        System.out.println("Turned off AC");
+    }
+
+    void setTemp(int temp) {
+        this.temp = temp;
+        System.out.println("Changed temp to " + temp);
+    }
+}
